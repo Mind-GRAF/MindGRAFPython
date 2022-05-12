fd34bf5bcf56d754aee127635d0819b12001a820
Antony <you@example.com>
2022-05-12 12:42:13 +0200

    RUI and Controller Untested and Non-Functiong(Probably)

---
+++ code/core/Infrence/RUI.py
@@ -0,0 +1,15 @@
+from RUI_Controller import RUI_Controller
+
+class RUI(RUI_Controller):
+    Subs =[]
+    Pcount=0
+    Ncount=0
+    Fns=False
+    InfType=""
+
+    def __init__(self):
+        self.Subs=list
+        self.Pcount=int
+        self.Ncount=int
+        self.Fns=bool
+        self.InfType=str
---
+++ code/core/Infrence/RUI_Controller.py
@@ -0,0 +1,8 @@
+from RUI import RUI
+
+class RUI_Controller:
+    RUI=[]
+    
+    
+    def __init__(self):
+        self.RUI=list
