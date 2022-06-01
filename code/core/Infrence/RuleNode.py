b78007108595115fdf3dd88b9544b939bbdb4d37
Antony <you@example.com>
2022-06-01 12:31:49 +0200

    final before rui

--- code/core/Infrence/RuleNode.py
+++ code/core/Infrence/RuleNode.py
@@ -7,71 +7,96 @@ from PropositionNode import PropositionNode
 
 
 class RuleNode(PropositionNode):
-    name = str
-    ant = list
-    cq = list
-    downCableS = list
-    min = int
-    max = int
+    name = ""
+    ant = []
+    cq = []
+    downCableS = []
+    min = 0
+    max = 0
+    source = ""
+    destination = ""
+    # this constructur is only for testing purposes input and output signatures should be taken from the Main constructor
+    def __init__(self, name, ant, cq, downCableS, source, destination):
+        self.source = source
+        self.destination = destination
+        self.name = name
+        self.ant = ant
+        self.cq = cq
+        self.downCables = downCableS
+        self.source = source
+        self.destination = destination
+        apply(self, "NULL")
 
+    def __init__2(self, name, ant, cq, downCableS, min, max, source, destination):
+        self.source = source
+        self.destination = destination
+        self.name = name
+        self.ant = ant
+        self.cq = cq
+        self.downCables = downCableS
+        self.min = min
+        self.max = max
 
-def __init__(self, downCableS):
-    self.downCableS = downCableS
-    for i in len(downCableS):
-        if downCableS(i).relation.getname() == "i":
-            self.name = "numerical_entailment"
-        elif downCableS(i).relation.getname() == "thresh":
-            self.name = "thresh"
-        elif downCableS(i).relation.getname() == "ant":
-            self.name = "or"
-        elif downCableS(i).relation.getname() == "max":
-            self.name = "andor"
-        else:
-            self.name = "and"
-
-    if self.name == "and":
-        for i in len(self.downCableS):
-            if downCableS(i).relation.getname() == "&ant":
-                self.ant = downCableS(i).getnodeset()
-            else:
-                self.cq = downCableS(i).getnodeset()
-    elif self.name == "or":
-        for i in len(self.downCableS):
-            if downCableS(i).relation.getname() == "ant":
-                self.ant = downCableS(i).getnodeset()
-            else:
-                self.cq = downCableS(i).getnodeset()
-    elif self.name == "numerical_entailment":
-        for i in len(self.downCableS):
-            if downCableS(i).relation.getname() == "&ant":
-                self.ant = downCableS(i).getnodeset()
-            elif downCableS(i).relation.getname() == "cq":
-                self.cq = downCableS(i).getnodeset()
-            else:
-                self.min = downCableS(i).i
-    elif self.name == "andor":
-        for i in len(self.downCableS):
-            if downCableS(i).relation.getname() == "arg":
-                self.ant = downCableS(i).getnodeset()
-                self.cq = downCableS(i).getnodeset()
+    # this is the main and offical constructor of Rule node
+    def __init__3(self, downCableS, source, destination):
+        self.source = source
+        self.destination = destination
+        self.downCableS = downCableS
+        for i in len(downCableS):
+            if downCableS(i).relation.getname() == "i":
+                self.name = "numerical_entailment"
+            elif downCableS(i).relation.getname() == "thresh":
+                self.name = "thresh"
+            elif downCableS(i).relation.getname() == "ant":
+                self.name = "or"
             elif downCableS(i).relation.getname() == "max":
-                self.max = downCableS(i).max
+                self.name = "andor"
             else:
-                self.min = downCableS(i).min
-    elif self.name == "thresh":
-        for i in len(self.downCableS):
-            if downCableS(i).relation.getname() == "arg":
-                self.ant = downCableS(i).getnodeset()
-                self.cq = downCableS(i).getnodeset()
-            elif downCableS(i).relation.getname() == "threshmax":
-                self.max = downCableS(i).threshmax
-            else:
-                self.min = downCableS(i).thresh
+                self.name = "and"
+
+        if self.name == "and":
+            for i in len(self.downCableS):
+                if downCableS(i).relation.getname() == "&ant":
+                    self.ant = downCableS(i).getnodeset()
+                else:
+                    self.cq = downCableS(i).getnodeset()
+        elif self.name == "or":
+            for i in len(self.downCableS):
+                if downCableS(i).relation.getname() == "ant":
+                    self.ant = downCableS(i).getnodeset()
+                else:
+                    self.cq = downCableS(i).getnodeset()
+        elif self.name == "numerical_entailment":
+            for i in len(self.downCableS):
+                if downCableS(i).relation.getname() == "&ant":
+                    self.ant = downCableS(i).getnodeset()
+                elif downCableS(i).relation.getname() == "cq":
+                    self.cq = downCableS(i).getnodeset()
+                else:
+                    self.min = downCableS(i).i
+        elif self.name == "andor":
+            for i in len(self.downCableS):
+                if downCableS(i).relation.getname() == "arg":
+                    self.ant = downCableS(i).getnodeset()
+                    self.cq = downCableS(i).getnodeset()
+                elif downCableS(i).relation.getname() == "max":
+                    self.max = downCableS(i).max
+                else:
+                    self.min = downCableS(i).min
+        elif self.name == "thresh":
+            for i in len(self.downCableS):
+                if downCableS(i).relation.getname() == "arg":
+                    self.ant = downCableS(i).getnodeset()
+                    self.cq = downCableS(i).getnodeset()
+                elif downCableS(i).relation.getname() == "threshmax":
+                    self.max = downCableS(i).threshmax
+                else:
+                    self.min = downCableS(i).thresh
 
 
 def apply(RuleNode, attuide):
     if isSupported(RuleNode, attuide):
-        if checkcombatiblesubstitutions(RuleNode.downCableS.antecedents):
+        if checkcombatiblesubstitutions(RuleNode.ant):
             match RuleNode.name:
                 case "or":
                     c = or_entailment(RuleNode)
@@ -83,6 +108,7 @@ def apply(RuleNode, attuide):
                     c = thresh(RuleNode)
                 case "numerical_entailment":
                     c = numerical_entailment(RuleNode)
+            print(c)
         if checkcombatiblesubstitutions(c):
             x = RuleNode.downCableS.antecedents.append(c)
             n = combinesupports(x)
@@ -106,3 +132,20 @@ def isSupported(x, zd):
 
 def report(c, supports, name, source, destination):
     print(c, supports, name, source, destination)
+
+
+# Testing stuff below (beware) ;
+RuleNode.__init__(
+    self=RuleNode,
+    name="or",
+    ant=["p", "q"],
+    cq=["p"],
+    downCableS=["ant", "cq"],
+    source="source",
+    destination="destination",
+)
+print(RuleNode.name)
+# RuleNode.__init__("and", ["p", "p->q"], ["q"], ["&ant", "cq"])
+# RuleNode.__init__("numerical_entailment", ["p", "p->q"], ["q"], ["&ant", "cq", "i"], 1)
+# RuleNode.__init__("andor", ["p", "p->q"], ["q"], ["args", "min", "max"], 1, 2)
+# RuleNode.__init__("thresh", [], [], ["args", "thresh", "threshmax"], 0, 1)
